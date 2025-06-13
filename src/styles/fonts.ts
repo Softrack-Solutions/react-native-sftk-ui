cf81b1ede8eab3b7c14df0db2d2902b001c3a139
Paulo Henrique Couto dos Reis <paulohcreis.contato@gmail.com>
2025-06-13 16:44:36 -0300

    theme

---
+++ src/styles/fonts.ts
@@ -0,0 +1,6 @@
+/** Enum para uso seguro com fontFamily */
+export enum EFonts {
+  Regular = 'UniviaPro-Regular',
+  Bold = 'UniviaPro-Bold',
+  Italic = 'UniviaPro-Italic',
+}
---
+++ src/styles/light.ts
@@ -0,0 +1,17 @@
+import type { Theme } from "../types/theme.type";
+import { EFonts } from "./fonts";
+import { EFontSizes } from "./fontSizes";
+
+export const lightTheme: Theme = {
+  colors: {
+    background: '',
+    text: '',
+    primary: '',
+  },
+  fonts: {
+    regular: EFonts.Regular,
+    bold: EFonts.Bold,
+    italic: EFonts.Italic,
+  },
+  fontSizes: EFontSizes,
+};
